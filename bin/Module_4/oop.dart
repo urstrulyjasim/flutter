ae99c5c91e0855433feec9ab89d3a15936c3c379
MR. JASIM <mohammadjasim229@gmail.com>
2025-09-12 22:35:53 +0600

    constructor and static keyword covered

--- bin/Module_4/oop.dart
+++ bin/Module_4/oop.dart
@@ -1,13 +1,15 @@
 void main()
 {
   Car myObj = Car(name:'toyota', model:'corolla');
-
+  print(Car.year);
 
 
 }
 class Car{
   String ? name;
   String ? model;
+  static int year = 2015;
+
 
   Car({required String name, required String model})
   {
